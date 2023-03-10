2616ab1d91e518d3dd2e956abfe242abb51de4d1
Koraytopuz <91742967+Koraytopuz@users.noreply.github.com>
2023-03-10 21:23:18 +0300

    Add files via upload

---
+++ iii.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+
+int a;
+cout<<"notu giriniz"<< endl;
+cin>>a;
+if(a>0&&a<=50)
+{
+	cout<<"f"<< endl;
+}
+else if(a>50&&a<=70)
+{
+	cout<<"c"<< endl;
+}
+else if(a>70&&a<=90)
+{
+	cout<<"b"<< endl;
+}
+else if(a>90&&a<=100)
+{
+	cout<<"a"<< endl;
+}
+
+}
